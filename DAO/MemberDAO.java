3c3751907fe2b6d5a23b558b6c257f0d7e6456b3
Huang <chaoyang0717@yahoo.com.tw>
2015-09-01 16:07:16 +0800

    Danny建立的Member DAO

---
+++ DAO/MemberDAO.java
@@ -0,0 +1,13 @@
+package order.member;
+
+import java.util.ArrayList;
+
+public interface MemberDAO {
+    public void add(Member member);
+    public void update(Member member);
+    public void remove(Member member);
+    public Member findById(String member_id);
+    public ArrayList<Member> getAllMembers();        
+    public ArrayList<Member> getRangeMembers(int start, int size);
+    public Member findByAccount(String member_account);
+}
